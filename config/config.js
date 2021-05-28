e1d60c43c2526c292799cef0d4f8b7d1a1168501
Bia <bia.irmal12@gmail.com>
2021-05-28 11:07:11 +0700

    fix: config.js

--- config/config.js
+++ config/config.js
@@ -21,6 +21,6 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOSTNAME,
-        dialect: 'mysql'
+        dialect: process.env.DB_DIALECT
     }
 }
